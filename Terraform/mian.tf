ab779fe7c3a5cd6e2e104065189138a37c760215
Mohammad Piyarji <136907810+Maddy9786@users.noreply.github.com>
2025-07-21 18:38:01 +0530

    mian.tf

--- Terraform/mian.tf
+++ Terraform/mian.tf
@@ -33,6 +33,10 @@ locals {
       ami           = data.aws_ami.ubuntu.id
       instance_type = "t3.micro"
     }
+    instance3 = {
+      ami           = data.aws_ami.ubuntu.id
+      instance_type = "t3.micro"
+    }
   }
 }
 
